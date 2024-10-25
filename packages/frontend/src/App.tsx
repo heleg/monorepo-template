7e3a3303f3b481d6659e359766bbe50a9baee54d
heleg <sandboxxx@ya.ru>
2024-10-25 07:25:00 +0400

    1

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import s from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={s.container}>
+      <h1>Monorepo Template</h1>
+    </div>
+  );
+};
+
+export default App;
