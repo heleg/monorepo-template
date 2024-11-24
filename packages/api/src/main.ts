b454c311ac3a6e41f95c62eb9ccfe03885a5a31a
heleg <sandboxxx@ya.ru>
2024-11-24 13:26:41 +0400

    1

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,12 +1,29 @@
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import * as session from "express-session";
 import { AppModule } from "./app.module";
 
-const PORT = 3000;
-
+// TODO: top level async???
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get<string>("PORT"));
+  const cookieSecret = configService.get<string>("COOKIE_SECRET");
+
+  if (!cookieSecret) {
+    throw new Error("COOKIE_SECRET is not set");
+  }
+
+  app.use(
+    session({
+      secret: cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
 }
-bootstrap().then(() => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+
+bootstrap();
