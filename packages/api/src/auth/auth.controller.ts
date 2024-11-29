960805184b54846452cf4b9aaea1c3b068f4ab06
heleg <sandboxxx@ya.ru>
2024-11-29 08:36:29 +0400

    1

--- packages/api/src/auth/auth.controller.ts
+++ packages/api/src/auth/auth.controller.ts
@@ -1,27 +1,4 @@
-import { Controller, Get, Request, UseGuards } from "@nestjs/common";
-import { AwsAuthGuard } from "./aws-auth.guard";
+import { Controller } from "@nestjs/common";
 
 @Controller("auth")
-export class AuthController {
-  @Get()
-  login() {
-    return {
-      url: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_2g6k7nq3q/.auth/login",
-    };
-  }
-
-  @Get("/me")
-  me(@Request() req: Request): string {
-    // @ts-ignore
-    return req.user;
-  }
-
-  @UseGuards(AwsAuthGuard)
-  @Get("/callback")
-  callback(@Request() req: Request) {
-    console.log("Request received at callback:", req);
-
-    // @ts-ignore
-    return req.user;
-  }
-}
+export class AuthController {}
--- packages/api/src/auth/auth.module.ts
+++ packages/api/src/auth/auth.module.ts
@@ -1,45 +1,4 @@
 import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { PassportModule } from "@nestjs/passport";
-import { discovery } from "openid-client";
-import { AuthController } from "./auth.controller";
 
-@Module({
-  controllers: [AuthController],
-  imports: [
-    PassportModule.register({
-      defaultStrategy: "aws",
-    }),
-  ],
-  providers: [
-    AwsAuthGuard,
-    {
-      provide: AwsStrategy,
-      useFactory: async (configService: ConfigService) => {
-        const clientId = configService.get<string>("AWS_COGNITO_CLIENT_ID");
-        const clientSecret = configService.get<string>(
-          "AWS_COGNITO_CLIENT_SECRET"
-        );
-        const server = configService.get<string>("AWS_COGNITO_SERVER");
-
-        if (!clientId || !clientSecret || !server) {
-          throw new Error(
-            "AWS_CLIENT_ID, AWS_CLIENT_SECRET, AWS_COGNITO_SERVER must be set"
-          );
-        }
-
-        const serverUrl = new URL(server);
-        const config = await discovery(serverUrl, clientId, clientSecret);
-
-        return new AwsStrategy({
-          config,
-          scope: "email openid profile",
-          callbackURL: "http://localhost:3388/auth/callback",
-          sessionKey: "aws",
-        });
-      },
-      inject: [ConfigService],
-    },
-  ],
-})
+@Module({})
 export class AuthModule {}
